1cc64647d72e0d7fbe25b3c6afab286d79a3af98
seeni <seeni@adaptivemedia.in>
2023-07-08 12:49:53 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { NomineeComponent } from './nominee/nominee.component';
+import { GuardianDetailsComponent } from './guardian-details/guardian-details.component';
+import { FormGridComponent } from './form-grid/form-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    NomineeComponent,
+    GuardianDetailsComponent,
+    FormGridComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
